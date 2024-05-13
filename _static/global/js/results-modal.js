a6bcc8ba3b686273516fd8118b579c471cea9998
Wesley Smith <yelsew414@gmail.com>
2024-05-13 11:37:12 -0600

    removed results breakdown

--- _static/global/js/results-modal.js
+++ _static/global/js/results-modal.js
@@ -38,26 +38,6 @@ let resultsModalComponent = {
                 </div>
                 <br>
                 
-                <template v-if="isOfficer">
-                    <p style="text-align: center; margin-top: 15px;">Breakdown</p>
-                    <div class="list-group" style="width: 450px; margin: auto;">
-                        
-                        <div v-if="resultsObject.fines != null" class="list-group-item list-group-item-primary">
-                            <div style="display: flex; justify-content: space-between;">
-                                <div>Amount earned ( <grain-image-component :size=20></grain-image-component>{{resultsObject.officer_bonus}} per civilian fined ):  </div>
-                                <div>Percentage earned per civilian fine</div>
-                                <div><grain-image-component :size=20></grain-image-component>{{ resultsObject.fines * income }}</div>
-                            </div>
-                        </div>
-                        <div v-if="resultsObject.reprimands != null" class="list-group-item list-group-item-primary">
-                            <div style="display: flex; justify-content: space-between;">
-                                <div>Amount lost ( <grain-image-component :size=20></grain-image-component>{{resultsObject.officer_reprimand_amount}} per reprimand ):</div>
-                                <div><grain-image-component :size=20></grain-image-component>{{ resultsObject.reprimands * resultsObject.officer_reprimand_amount * -1 }}</div>
-                            </div>
-                        </div>   
-                        
-                    </div>
-                </template>
 
             </div>
             
